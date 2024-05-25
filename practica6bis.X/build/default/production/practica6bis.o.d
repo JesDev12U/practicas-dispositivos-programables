36a29b5c5988a206b0f86d2ac65ff2faf4eb723a
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-05-24 22:19:14 -0600

    Practica 6bis

---
+++ practica6bis.X/build/default/production/practica6bis.o.d
@@ -0,0 +1,4 @@
+build/default/production/practica6bis.o:  \
+	C:/Users/jesus/Desktop/PracticasDispositivosProgramables/practica6bis.X/practica6bis.asm  \
+	C:/Users/jesus/Desktop/PracticasDispositivosProgramables/practica6bis.X/header.inc  \
+
