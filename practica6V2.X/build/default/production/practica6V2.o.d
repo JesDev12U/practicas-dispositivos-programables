dc28e8419781164a3ab49b310491ab91dd306ce6
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-05-24 23:26:22 -0600

    Practica 6 y 6bis Version 2, sin quedarse pensando

---
+++ practica6V2.X/build/default/production/practica6V2.o.d
@@ -0,0 +1,4 @@
+build/default/production/practica6V2.o:  \
+	C:/Users/jesus/Desktop/PracticasDispositivosProgramables/practica6V2.X/practica6V2.asm  \
+	C:/Users/jesus/Desktop/PracticasDispositivosProgramables/practica6V2.X/header.inc  \
+
---
+++ practica6bisV2.X/build/default/production/practica6bisV2.o.d
@@ -0,0 +1,4 @@
+build/default/production/practica6bisV2.o:  \
+	C:/Users/jesus/Desktop/PracticasDispositivosProgramables/practica6bisV2.X/practica6bisV2.asm  \
+	C:/Users/jesus/Desktop/PracticasDispositivosProgramables/practica6bisV2.X/header.inc  \
+
